f95323b3725b67896d59128584fa2fdfef11beac
Bulelani Gabonewe <bulelanigabonewe@gmail.com>
2025-02-25 10:31:53 +0200

    Create the get human choice function
    Function prompts the user for a choice and validates whether they entered a valid value. Else, it just keeps on prompting.

--- scripts/rock-paper-scissors.js
+++ scripts/rock-paper-scissors.js
@@ -4,3 +4,17 @@ function getComputerChoice(){
     return choices.at(index);
 }
 
+
+
+function getHumanChoice(){
+    let choice;
+    do{
+        choice = prompt("Please enter your choice between 'Rock', 'Paper' or 'Scissors'");
+    }
+    while (!['rock', 'paper', 'scissors'].includes(choice.toLocaleLowerCase()))
+
+    return choice;
+}
+
+
+console.log(getHumanChoice());
