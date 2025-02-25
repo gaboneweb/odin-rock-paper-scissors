5ddd6ea05d46b1ad61a5b8f9c88313e75a6e9ea0
Bulelani Gabonewe <bulelanigabonewe@gmail.com>
2025-02-25 11:33:20 +0200

    Create game loop and logic for each round

--- scripts/rock-paper-scissors.js
+++ scripts/rock-paper-scissors.js
@@ -1,3 +1,13 @@
+
+let humanScore = 0;
+let computerScore = 0;
+
+
+function capitalize(word){
+    return word.charAt(0).toUpperCase() + word.slice(1);
+}
+
+
 function getComputerChoice(){
     let choices = ["rock", "paper", "scissors"];
     let index = Math.floor((Math.random() * choices.length - 1) + 1);
@@ -5,16 +15,59 @@ function getComputerChoice(){
 }
 
 
-
 function getHumanChoice(){
     let choice;
     do{
         choice = prompt("Please enter your choice between 'Rock', 'Paper' or 'Scissors'");
     }
-    while (!['rock', 'paper', 'scissors'].includes(choice.toLocaleLowerCase()))
+    while (!['rock', 'paper', 'scissors'].includes(choice.toLowerCase()))
 
-    return choice;
+    return choice.toLowerCase();
 }
 
+function checkIfWon(currentPlayerChoice, opponentChoice){
+        return (currentPlayerChoice === 'rock' && opponentChoice === 'scissors')
+        || (currentPlayerChoice === 'scissors' && opponentChoice === 'paper')
+        || (currentPlayerChoice === 'paper' && opponentChoice === 'rock');
+}
 
-console.log(getHumanChoice());
+function playRound(humanChoice, computerChoice) {
+    if(humanChoice.toLowerCase() === computerChoice.toLowerCase()){
+        console.log("Its a draw!")
+    }else if(checkIfWon(computerChoice, humanChoice)){
+        console.log(`You lose!${capitalize(computerChoice)}  beats ${capitalize(humanChoice)}.`);
+        computerScore++;
+    }else if(checkIfWon(humanChoice, computerChoice)){
+        console.log(`You Win!${capitalize(humanChoice)}  beats ${capitalize(computerChoice)}.`);
+        humanScore++;
+    }
+}
+
+function overallWinner(){
+    if(humanScore > computerScore){
+            console.log(`You beat the computer!!Your score ${humanScore} to computer score ${computerScore}.`)
+    }else{
+        console.log(`You lost!!Computor score ${computerScore}, your score ${humanScore}.`)
+    }
+}
+
+function playGame(){
+    humanScore = 0;
+    computerScore = 0;
+    let currentRound = 0;
+
+
+    while( currentRound < 5){
+        const computerSelection = getComputerChoice();
+        const humanSelection = getHumanChoice();
+        playRound(humanSelection, computerSelection);
+        currentRound++;
+    }
+
+    overallWinner();
+}
+  
+
+  
+
+playGame()
